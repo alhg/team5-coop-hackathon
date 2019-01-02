f66b9d9890ba85cdac93c02cd2558e4ade686150
Wei-Han <chouweihan@gmail.com>
2019-01-02 12:32:08 -0800

    download button

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
     $('#js-upload-submit').click(function(){
       console.log("hello");
     });
+
+    $('#js-button-download').click(function(){
+      console.log("downloaded!");
+      var now = document.getElementById("textAreaID").value;
+    this.href = "data:text/plain;charset=UTF-8,"  + encodeURIComponent(now);
+    });
 });
